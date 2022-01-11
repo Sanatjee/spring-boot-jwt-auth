0a1ac17d9051895b1d30a8c5edcadfeecef7c978
Sanat Gawade <sanat.leo9@gmail.com>
2022-01-11 23:40:20 +0530

    Custom Auth filter added along with security config

--- src/main/java/com/enigma/userservice/service/UserServiceImpl.java
+++ src/main/java/com/enigma/userservice/service/UserServiceImpl.java
@@ -6,16 +6,39 @@ import com.enigma.userservice.repo.RoleRepo;
 import com.enigma.userservice.repo.UserRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @AllArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if(user == null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }else{
+            log.info("User found in the database");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), authorities);
+    }
+
     @Override
     public User saveUser(User user) {
         log.info("Saving {} user to the database", user.getName());
@@ -48,4 +71,6 @@ public class UserServiceImpl implements UserService{
         log.info("fetching all user from database");
         return userRepo.findAll();
     }
+
+
 }
